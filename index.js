53ad23138e3f4c1712b68a5533f36e06dc43742a
msj102525 <95277177+msj102525@users.noreply.github.com>
2022-03-18 19:30:42 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gallery=document.querySelector('.gallery');
+const galleryLi=document.querySelectorAll('.gallery>ul>li');
+const arrBg=[];
+
+let i=-1
+
+function autoGallery(){
+    i++;
+
+    const gap=galleryLi[1].offsetLeft-galleryLi[0].offsetLeft;
+    const goto=(i*-gap)+'px';
+    console.log(`gap -> ${gap}, goto -> ${goto}`);
+
+    gallery.style.left=goto;
+    gallery.style.transition='all 0.5s'
+
+    if(i>=galleryLi.length-1) i=-1;
+    
+}
+setInterval(autoGallery,2000);
+// (function(){
+//     autoGallery();
+// })();
+(()=>{
+    autoGallery();
+})();
+
+
+for(let i=0;i<galleryLi.length;i++){
+    arrBg.push(`url(img/sec${i}.jpg) no-repeat 50%/cover`);
+    galleryLi[i].style.background=arrBg[i];
+}
